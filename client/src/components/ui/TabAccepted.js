c14888fb6f3b19eef16daf6d8382a949701cc1ee
paulalbert1 <paa2013@med.cornell.edu>
2020-11-28 06:47:07 -0500

    Update TabAccepted.js

--- client/src/components/ui/TabAccepted.js
+++ client/src/components/ui/TabAccepted.js
@@ -129,11 +129,11 @@ export class TabAccepted extends Component {
                                 addPublication = true
                             }
                             // title
-                            if (publication.title.toLowerCase().includes(thisObject.state.search.toLowerCase())) {
+                            if (publication.title !== undefined && publication.title.toLowerCase().includes(thisObject.state.search.toLowerCase())) {
                                 addPublication = true;
                             }
                             // journal
-                            if (publication.journal.toLowerCase().includes(thisObject.state.search.toLowerCase())) {
+                            if (publication.journal !== undefined && publication.journal.toLowerCase().includes(thisObject.state.search.toLowerCase())) {
                                 addPublication = true;
                             }
                             //issn
