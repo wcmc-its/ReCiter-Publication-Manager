b54a241975367f52b603688a1e339e6a8bf473a0
sarbajitdutta <sarbajitdutta35@gmail.com>
2021-10-04 13:50:07 -0400

    userfeedback controller for all feedback apis

--- controllers/userfeedback.controller.ts
+++ controllers/userfeedback.controller.ts
@@ -1,3 +1,4 @@
+import { NextApiRequest } from 'next';
 import { reciterConfig } from '../config/local'
 
 export async function deleteUserFeedback(uid: string | string[])  {
@@ -9,11 +10,79 @@ export async function deleteUserFeedback(uid: string | string[])  {
             'User-Agent': 'reciter-pub-manager-server'
         },
     })
-        .then((res) => {
-            return res.statusText
+        .then(async (res) => {
+            let responseText = await res.text()
+            return {
+                statusCode: res.status,
+                statusText: responseText
+            }
         })
         .catch((error) => {
             console.log('ReCiter Delete User Feedback api is not reachable: ' + error)
-            return error
+            return {
+                statusCode: error.status,
+                statusText: error
+            }
         });
 }
+
+export async function findUserFeedback(uid: string | string[])  {
+    
+    return fetch(`${reciterConfig.reciter.reciterUserFeedbackEndpoints.findUserFeedback}` +'?uid=' + uid, {
+         method: "GET",
+         headers: {
+             'api-key': reciterConfig.reciter.adminApiKey,
+             'User-Agent': 'reciter-pub-manager-server'
+         },
+     })
+         .then(async (res) => {
+            let responseText = await res.text()
+             if(res.status !== 200) {
+                 return {
+                    statusCode: res.status,
+                    statusText: responseText
+                }
+             } else  {
+                let data: any = await res.json()
+                return {
+                    statusCode: res.status,
+                    statusText: data
+                }
+             }
+         })
+         .catch((error) => {
+             console.log('ReCiter Find User Feedback api is not reachable: ' + error)
+             return {
+                statusCode: error.status,
+                statusText: error
+            }
+         });
+ }
+
+ export async function saveUserFeedback(req: NextApiRequest, uid: string | string[])  {
+    
+    return fetch(`${reciterConfig.reciter.reciterUserFeedbackEndpoints.saveUserFeedback}` +'?uid=' + uid, {
+         method: "POST",
+         headers: {
+             'api-key': reciterConfig.reciter.adminApiKey,
+             'User-Agent': 'reciter-pub-manager-server',
+             'Content-Type': 'application/json',
+             'Content-Length' : req.body.length
+         },
+         body: JSON.stringify(req.body)
+     })
+         .then(async (res) => {
+            let data: any = await res.json()
+            return {
+                statusCode: res.status,
+                statusText: data
+            }
+         })
+         .catch((error) => {
+             console.log('ReCiter Save User Feedback api is not reachable: ' + error)
+             return {
+                statusCode: error.status,
+                statusText: error
+            }
+         });
+ }
