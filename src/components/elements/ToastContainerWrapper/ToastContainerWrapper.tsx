a5263d6a6bef7c5e30c7ca01070deb5d398a4890
sarbajitdutta <sarbajitdutta35@gmail.com>
2021-10-14 11:44:17 -0400

    toast container wrapper

---
+++ src/components/elements/ToastContainerWrapper/ToastContainerWrapper.tsx
@@ -0,0 +1,8 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const ToastContainerWrapper = () => {
+    return <ToastContainer />
+}
+
+export default ToastContainerWrapper
