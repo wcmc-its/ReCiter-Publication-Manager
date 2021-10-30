1225e936f32462a6b5b4a0cb1f461ebdfd74d43f
sarbajitdutta <sarbajitdutta35@gmail.com>
2021-10-30 09:42:37 -0400

    add session check for page

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,10 +1,30 @@
 import Search from '../../components/elements/Search/Search'
 import { AppLayout } from "../../components/layouts/AppLayout"
+import { getSession } from "next-auth/client"
+
+export async function getServerSideProps(ctx) {
+    const session = await getSession(ctx);
+
+    if (!session || !session.data) {
+        return {
+            redirect: {
+                destination: "/login",
+                permanent: false,
+            },
+        };
+    }
+
+    return {
+        props: {
+            session: session,
+        },
+    };
+}
 
 const SearchPage = () => {
     return (
         <>  
-                <Search />
+                {/* <Search /> */}
         </>
     )
 }
