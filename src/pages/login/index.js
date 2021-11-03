5d3957013b5670f9b115e6e336ace83c0479cc39
sarbajitdutta <sarbajitdutta35@gmail.com>
2021-11-03 13:14:31 -0400

    handle session for login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,4 +1,24 @@
 import Login from '../../components/elements/Login/Login'
+import { getSession } from "next-auth/client"
+
+export async function getServerSideProps(ctx) {
+    const session = await getSession(ctx);
+
+    if (session && session.data) {
+        return {
+            redirect: {
+                destination: "/search",
+                permanent: false,
+            },
+        };
+    }
+
+    return {
+        props: {
+            session: session,
+        },
+    };
+}
 
 const LoginPage = () => {
     return (
