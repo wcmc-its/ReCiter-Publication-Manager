37dde899b3808b5a791cd8300dd4a5da3e245dd5
sarbajitdutta <sarbajitdutta35@gmail.com>
2021-11-03 13:14:41 -0400

    enbale search

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -24,7 +24,7 @@ export async function getServerSideProps(ctx) {
 const SearchPage = () => {
     return (
         <>
-                {/* <Search /> */}
+            <Search />
         </>
     )
 }
