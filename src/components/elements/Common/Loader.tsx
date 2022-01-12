e9de54ceb88036c62558a6ee54ec2e30c7a53079
judygab <ivditigabeskiria@gmail.com>
2022-01-12 16:31:44 -0500

    fixed Loader bug

--- src/components/elements/Common/Loader.tsx
+++ src/components/elements/Common/Loader.tsx
@@ -2,7 +2,7 @@ import { Spinner } from "react-bootstrap";
 
 const Loader = () => {
   return (
-    <div className="d-flex justify-content-center align-items-center"><Spinner animation="border" variant="danger" style={{ height: '5rem', width: '5rem'}} size="lg"/></div>
+    <div className="d-flex justify-content-center align-items-center"><Spinner animation="border" variant="danger" style={{ height: '5rem', width: '5rem'}}/></div>
   )
 }
 
