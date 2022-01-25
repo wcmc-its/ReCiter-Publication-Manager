df037b8757379a5ca55987b58b142e801cadc623
sarbajitdutta <sarbajitdutta35@gmail.com>
2022-01-25 13:28:50 -0500

    change to js

--- src/pages/api/saml/assert.js
+++ src/pages/api/saml/assert.js
@@ -1,17 +1,18 @@
 import saml2 from "saml2-js"
-import type saml2js from "saml2-js"
 import axios from "axios"
-import type { NextApiRequest, NextApiResponse } from 'next'
+//import type { NextApiRequest, NextApiResponse } from 'next'
 import { reciterSamlConfig }  from "../../../../config/saml"
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req, res) {
     if (req.method === "POST") {
         const { data, headers } = await axios.get("/api/auth/csrf", {
             baseURL: "https://" + req.headers.host,
         });
+        console.log(data)
         const { csrfToken } = data;
 
         const encodedSAMLBody = encodeURIComponent(JSON.stringify(req.body));
+        console.log(encodedSAMLBody)
 
         res.setHeader("set-cookie", headers["set-cookie"] ?? "");
         return res.send(
@@ -30,10 +31,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }
 
     const sp = new saml2.ServiceProvider(reciterSamlConfig.saml_options);
-    const createLoginRequestUrl = (idp: saml2js.IdentityProvider, options = {}) =>
-        new Promise<string>((resolve, reject) => {
+    const createLoginRequestUrl = (idp, options = {}) =>
+        new Promise((resolve, reject) => {
             sp.create_login_request_url(idp, options, (error, loginUrl) => {
                 if (error) {
+                    console.log(error)
                     reject(error);
                 }
                 resolve(loginUrl);
@@ -43,6 +45,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     try {
         const idp = new saml2.IdentityProvider(reciterSamlConfig.saml_idp_options);
         const loginUrl = await createLoginRequestUrl(idp);
+        console.log(loginUrl)
 
         return res.redirect(loginUrl);
     } catch (error) {
