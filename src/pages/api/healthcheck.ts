bf16b556e89961978a8970d4caa4fbd760f04bcc
sarbajitdutta <sarbajitdutta35@gmail.com>
2022-01-25 12:47:45 -0500

    added healthcheck api

--- src/pages/api/healthcheck.ts
+++ src/pages/api/healthcheck.ts
@@ -1,13 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  success: string,
+  DB: string
 }
 
 export default function handler(
-  req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ success: '1', DB: '1'})
 }
