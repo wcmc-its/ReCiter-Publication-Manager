12284b2bb66568b2e7ad401aedf409eb7c6ec295
judygab <ivditigabeskiria@gmail.com>
2022-02-09 10:41:09 -0500

    added custom error page

---
+++ src/pages/_error.js
@@ -0,0 +1,11 @@
+import Error from '../components/elements/Error/Error'
+
+const ErrorPage = () => {
+    return (
+        <>
+            <Error />
+        </>
+    )
+}
+
+export default ErrorPage
