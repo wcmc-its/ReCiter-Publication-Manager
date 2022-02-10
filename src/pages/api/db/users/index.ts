aec696c4e05a41c5dda65ebfa25b9afe77b95b21
sarbajitdutta <sarbajitdutta35@gmail.com>
2022-02-10 13:28:54 -0500

    make person api post request

--- src/pages/api/db/users/index.ts
+++ src/pages/api/db/users/index.ts
@@ -4,10 +4,8 @@ import { Person } from '../../../../db/models/Person'
 
 export default async function handler(req: NextApiRequest,
     res: NextApiResponse<Person | string>) {
-    if (req.method === "GET") {
-        const offset = req.query.offset as string
-        const limit = req.query.limit as string
-        await findAll(req, res, offset, limit)
+    if (req.method === "POST") {
+        await findAll(req, res)
     } else {
         // Default this to a bad request for now
         res.status(400).send('Bad request')
