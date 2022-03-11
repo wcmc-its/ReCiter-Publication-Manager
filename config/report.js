a1a9f915cb5a7f8f1905f5f05792a04f01e85818
judygab <ivditigabeskiria@gmail.com>
2022-03-11 10:34:11 -0500

    added config file for reports

---
+++ config/report.js
@@ -0,0 +1,15 @@
+export const reportConfig = {
+  authorFilters: {
+    author: true,
+    organization: true,
+    institution: true,
+    personType: true,
+    order: true
+  },
+  articleFilters: {
+    date: true,
+    type: true,
+    journal: true,
+    journalRank: true
+  }
+}
