a5b65d3c37cf1786750d69b701dc13bc9f0b36f1
sarbajitdutta <sarbajitdutta35@gmail.com>
2022-04-08 10:18:20 -0400

    publication search types for apis

--- types/publication.report.search.d.ts
+++ types/publication.report.search.d.ts
@@ -4,18 +4,34 @@ export type PublicationSearchFilter = {
         orgUnits?: Array<string>,
         institutions?: Array<string>,
         personTypes?: Array<string>,
-        authorPosition: Array<AuthorPosition>
-        datePublicationAddedToEntrezLowerBound: string,
-        datePublicationAddedToEntrezUpperBound: string,
-        publicationTypeCanonical: Array<string>,
-        journalTitleVerbose: Array<string>,
-        journalImpactScoreLowerBound: number,
-        journalImpactScoreUpperBound: number
+        authorPosition?: Array<AuthorPosition>
+        datePublicationAddedToEntrezLowerBound?: string,
+        datePublicationAddedToEntrezUpperBound?: string,
+        publicationTypeCanonical?: Array<string>,
+        journalTitleVerbose?: Array<string>,
+        journalImpactScoreLowerBound?: number,
+        journalImpactScoreUpperBound?: number
     },
-    limit?: number,
-    offset?: number
+    sort?: PublicationSort,
+    limit: number,
+    offset: number
 }
 export enum AuthorPosition {
     first,
     last
+}
+
+export type PublicationAuthorSearchFilter = {
+    pmids: Array<number>,
+    personIdentifers?: Array<string>
+}
+
+export type PublicationSort = {
+    datePublicationAddedToEntrez?: boolean
+    journalImpactScore1?: boolean
+    percentileNIH?: boolean
+    citationCountNIH?: boolean
+    trendingPubsScore?: boolean
+    readersMendeley?: boolean
+    publicationDateStandarized?: boolean
 }
