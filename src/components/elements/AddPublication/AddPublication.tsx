a6974929458cff2afd223c0df77b69d2a4730705
Mahender Reddy Jangari <mjangari@gcs.com>
2022-07-26 22:52:13 +0530

    Fixed Add PubMed issues, redirect issue to error page, Other mockup screen changes

--- src/components/elements/AddPublication/AddPublication.tsx
+++ src/components/elements/AddPublication/AddPublication.tsx
@@ -28,16 +28,12 @@ const AddPublication: FunctionComponent<FuncProps> = (props) => {
                     className={`btn btn-success ${publicationStyles.publicationAccept}`}
                     onClick={acceptPublication}
                     
-                > <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check" viewBox="0 0 16 16">
-                <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
-              </svg>Accept
+                > <svg className="MuiSvgIcon-root MuiSvgIcon-fontSizeSmall css-ptiqhd-MuiSvgIcon-root" focusable="false" viewBox="0 0 24 24" aria-hidden="true" data-testid="CheckIcon"><path d="M9 16.17 4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"></path></svg>Accept
                 </button>
                 <button
                     className={`btn btn-danger ${publicationStyles.publicationUndo}`}
                     onClick={rejectPublication}
-                ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16">
-                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-              </svg>Reject
+                ><svg className="MuiSvgIcon-root MuiSvgIcon-fontSizeSmall css-ptiqhd-MuiSvgIcon-root" focusable="false" viewBox="0 0 24 24" aria-hidden="true" data-testid="ClearIcon"><path d="M19 6.41 17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path></svg>Reject
                 </button>
             </div>
         </td>
