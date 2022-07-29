e341e5d69e4519347d90b3ff5275c6e5dafa848a
Mahender Reddy Jangari <mjangari@gcs.com>
2022-07-29 18:40:18 +0530

    Corrected CSS issue on search page

--- src/components/elements/AddPublication/AddPublication.tsx
+++ src/components/elements/AddPublication/AddPublication.tsx
@@ -31,7 +31,7 @@ const AddPublication: FunctionComponent<FuncProps> = (props) => {
                 > <svg className="MuiSvgIcon-root MuiSvgIcon-fontSizeSmall css-ptiqhd-MuiSvgIcon-root" focusable="false" viewBox="0 0 24 24" aria-hidden="true" data-testid="CheckIcon"><path d="M9 16.17 4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"></path></svg>Accept
                 </button>
                 <button
-                    className={`btn btn-danger ${publicationStyles.publicationUndo}`}
+                    className={`btn btn-danger ${publicationStyles.publicationReject}`}
                     onClick={rejectPublication}
                 ><svg className="MuiSvgIcon-root MuiSvgIcon-fontSizeSmall css-ptiqhd-MuiSvgIcon-root" focusable="false" viewBox="0 0 24 24" aria-hidden="true" data-testid="ClearIcon"><path d="M19 6.41 17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path></svg>Reject
                 </button>
