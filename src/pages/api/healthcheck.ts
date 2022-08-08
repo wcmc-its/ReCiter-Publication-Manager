c556fa1f33c6fe17d03bb5850aff007cc7f8b883
jbrutus3 <ajb4001@med.cornell.edu>
2022-08-08 13:06:14 -0400

    update docker with new env variables

--- src/pages/api/healthcheck.ts
+++ src/pages/api/healthcheck.ts
@@ -3,12 +3,13 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   success: string,
-  DB: string
+  DB: string,
+  test: string
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).send({ success: '1', DB: '1'})
+  res.status(200).send({ success: '1', DB: '1', test: `${process.env.RECITER_IDENITY_BY_UID} ${process.env.NEXT_PUBLIC_RECITER_TOKEN_SECRET}`})
 }
