651b00167ffeb9814ad4b2244c98ac7ceb43da75
Mahender Reddy Jangari <mjangari@gcs.com>
2022-10-11 19:45:51 +0530

    SAML redirection testing

--- src/utils/loginHelper.ts
+++ src/utils/loginHelper.ts
@@ -1,4 +1,5 @@
-export const getSigninUrl = () => {
+export const getSigninUrl = (sessionData) => {
+    console.log("getsignURL called from LoginHelper********************", sessionData);
     return process.env.LOGIN_PROVIDER !== "SAML"
         ? `${window.location.origin}/login`
         : `${window.location.origin}/api/saml/assert?callbackUrl=/search`;
