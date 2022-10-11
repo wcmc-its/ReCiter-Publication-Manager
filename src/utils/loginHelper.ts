44bdf5c42b324886ff0271a4e945c55068c20976
Mahender Reddy Jangari <mjangari@gcs.com>
2022-10-11 07:58:25 +0530

    saml authentication

--- src/utils/loginHelper.ts
+++ src/utils/loginHelper.ts
@@ -1,4 +1,34 @@
+import { signIn,getSession } from "next-auth/client"
+import { allowedPermissions } from "../utils/constants";
+import Router from "next/router"
 export const getSigninUrl = () => {
+
+    const session =  getSession();
+    console.log("session parameters******************************",session);
+    /*let userPermissions = JSON.parse(session.data.userRoles);
+    let userName = session.data.username;
+    let personIdentifier = userPermissions && userPermissions.length > 0 ? userPermissions[0].personIdentifier : ""
+    console.log("userPermission***********************",userPermissions);*/
+    console.log("allowedPermissions***********************",allowedPermissions);
+   /* getSession().then((session) => 
+    {
+        if (session) 
+        {
+            let userPermissions = JSON.parse(session.data.userRoles);
+            let userName = session.data.username;
+            let personIdentifier = userPermissions && userPermissions.length > 0 ? userPermissions[0].personIdentifier : ""
+            console.log("userPermission***********************",userPermissions);
+            if(process.env.LOGIN_PROVIDER !== "SAML")
+                return `${window.location.origin}/login`;
+            else if((userPermissions.some(role => role.roleLabel === allowedPermissions.Curator_Self)) && userName)  
+               return `${window.location.origin}/api/saml/assert?callbackUrl = /curate/${personIdentifier}`;
+            else
+            {
+                return `${window.location.origin}/api/saml/assert?callbackUrl=/search`;
+            }
+        
+        } 
+    });*/
     return process.env.LOGIN_PROVIDER !== "SAML"
         ? `${window.location.origin}/login`
         : `${window.location.origin}/api/saml/assert?callbackUrl=/search`;
