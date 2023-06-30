8d7c978c83022d464316c6c790ed6308dbf252db
Mahender Reddy Jangari <mrj4001@med.cornell.edu>
2023-06-30 13:49:48 +0530

    Capturing firstName, lastName and email from SAML and storing it to AdminUsers

--- src/pages/api/auth/[...nextauth].jsx
+++ src/pages/api/auth/[...nextauth].jsx
@@ -37,9 +37,9 @@ const createAdminUserWithCWID = async(cwid,samlEmail,samlFirstName,samlLastName)
     console.log('All attributes**********************',cwid,samlEmail,samlFirstName,samlLastName);
     const createdAdminUser = await findOrCreateAdminUsers(cwid,samlEmail,samlFirstName,samlLastName)
     console.log('adminUser after creating1*************************',createdAdminUser);
-    console.log('adminUser after creating2*************************',createdAdminUser.toJSON());
+   // console.log('adminUser after creating2*************************',createdAdminUser.toJSON());
     console.log('adminUser after creating3*************************',createdAdminUser);
-    if(createdAdminUser || (createdAdminUser!=null && createdAdminUser!='' && createdAdminUser!=undefined))
+    if(createdAdminUser && Object.keys(createdAdminUser) && Object.keys(createdAdminUser).length > 0)
     {
         Console.log('AdminUser inside condition****************************',adminUser);
         let [assignedRoles, userRoles] = await Promise.all([grantDefaultRolesToAdminUser(adminUser), findUserPermissions(cwid, "cwid")]);
