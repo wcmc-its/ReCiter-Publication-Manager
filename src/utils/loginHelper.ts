ab973beab6e9e1a8b82fc50260005dd7538c295d
Mahender Reddy Jangari <mrj4001@med.cornell.edu>
2023-07-03 09:26:06 +0530

    Capturing firstName, lastName and email from SAML and storing it to AdminUsers

--- src/utils/loginHelper.ts
+++ src/utils/loginHelper.ts
@@ -1,5 +1,6 @@
 export const getSigninUrl = () => {
-    return (process.env.LOGIN_PROVIDER && process.env.NEXT_PUBLIC_LOGIN_PROVIDER !== "SAML")
+    console.log('LOGIN PROVIDER**********************************',process.env.NEXT_PUBLIC_LOGIN_PROVIDER);
+    return (process.env.NEXT_PUBLIC_LOGIN_PROVIDER && process.env.NEXT_PUBLIC_LOGIN_PROVIDER !== "SAML")
         ? `${window.location.origin}/login`
         : `${window.location.origin}/api/saml/assert?callbackUrl=/search`;
 };
