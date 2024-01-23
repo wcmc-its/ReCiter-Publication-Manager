c3110264ffa7d20e21eb40768f41e19791c2cf43
mrj4001 <mrj4001@med.cornell.edu>
2024-01-23 09:48:15 +0530

    Refactor the sequelize code to support the AWS Secrets Manager

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,20 +1,43 @@
-import { Sequelize } from "sequelize"
+import { Sequelize} from "sequelize";
+import  {getSecret} from '../utils/secretsManager';
 
-const sequelize = new Sequelize(
-    process.env.RECITER_DB_NAME || "",
-    process.env.RECITER_DB_USERNAME || "",
-    process.env.RECITER_DB_PASSWORD,
-    {
-        dialect: 'mysql',
-        host: process.env.RECITER_DB_HOST,
-        port: 3306,
-        pool: {
-            max: 20,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
-        },
-        //logging: false
-})
-
-export default sequelize
+   
+let sequelize:any ='';
+(async () => {
+    try {
+        // Retrieve secrets from AWS Secrets Manager
+            let dbSecrets:any ='';
+            const reciterPubNotifierSecretLoc = process.env.RECITER_PUB_NOTIFIER_SECRET_LOC || '';
+            if(reciterPubNotifierSecretLoc && reciterPubNotifierSecretLoc =='AWS')
+            {
+                const reciterPubSecretManager = process.env.RECITER_PUB_SECRET_MANAGER || '';
+                 dbSecrets = await getSecret( reciterPubSecretManager);
+            }
+            const { RECITER_DB_NAME,RECITER_DB_USERNAME,RECITER_DB_PASSWORD,RECITER_DB_HOST,RECITER_DB_REGION, RECITER_DB_PORT } = dbSecrets;
+            sequelize = new Sequelize(
+            RECITER_DB_NAME || process.env.RECITER_DB_NAME || "",
+            RECITER_DB_USERNAME || process.env.RECITER_DB_USERNAME || "",
+            RECITER_DB_PASSWORD || process.env.RECITER_DB_PASSWORD || "",
+            {
+                dialect: 'mysql',
+                dialectOptions: {
+                    connectTimeout: 120000, // 60 seconds
+                },
+                host: RECITER_DB_HOST || process.env.RECITER_DB_HOST  ,
+                port:  RECITER_DB_PORT || 3306,
+                pool: {
+                    max: 20,
+                    min: 0,
+                    acquire: 30000,
+                    idle: 10000
+                }
+            }
+        );
+        
+    }
+    catch (error) {
+        console.error('Error initializing Sequelize:', error);
+        throw error;
+        }
+})();
+export default sequelize;
--- src/utils/emailUtilityHelper.js
+++ src/utils/emailUtilityHelper.js
@@ -2,16 +2,33 @@ import NodeMailer from "nodemailer";
 
 
 export function sendEmailNotification(mailOptions) {
+
+  let dbSecrets ='';
+  (async () => {
+    try {
+          const reciterPubNotifierSecretLoc = process.env.RECITER_PUB_NOTIFIER_SECRET_LOC || '';
+          if(reciterPubNotifierSecretLoc && reciterPubNotifierSecretLoc =='AWS')
+          {
+                const reciterPubSecretManager = process.env.RECITER_PUB_SECRET_MANAGER || '';
+                dbSecrets = await getSecret(reciterPubSecretManager);
+          }
+          const { SMTP_HOST_NAME,SMTP_ADMIN_EMAIL,SMTP_PASSWORD,SMTP_USER,SMTP_PORT, SMTP_SECURE } = dbSecrets;
+        }
+        catch (error) {
+            console.error('Error initializing Sequelize:', error);
+            throw error;
+            }
+    })();        
   return new Promise((resolve,reject)=>{
     let transporter = NodeMailer.createTransport(({
-      host: process.env.SMTP_HOST_NAME,
-      port: process.env.NODE_ENV === "production" ? 465 : 25,
-      secure: process.env.NODE_ENV === "production" ? true : false,
+      host: dbSecrets.SMTP_HOST_NAME || process.env.SMTP_HOST_NAME,
+      port: process.env.NODE_ENV === "production" ? dbSecrets.SMTP_PORT : 25,
+      secure: process.env.NODE_ENV === "production" ? dbSecrets.SMTP_SECURE : false,
       logger: true,
       debug: true,
       auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASSWORD
+        user: dbSecrets.SMTP_USER || process.env.SMTP_USER,
+        pass: dbSecrets.SMTP_PASSWORD || process.env.SMTP_PASSWORD
       },
       tls: {
         rejectUnAuthorized: process.env.NODE_ENV === "production" ? true : false,
