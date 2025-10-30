dbcc6746e22855f89126038ec31c2b65684077eb
mrj4001 <mrj4001@med.cornell.edu>
2025-10-30 19:03:10 +0530

    made somemore changes to make it work with Next Auth 4 and NextJs 14

--- src/pages/api/saml/assert.js
+++ src/pages/api/saml/assert.js
@@ -2,7 +2,7 @@ import saml2 from "saml2-js"
 import axios from "axios"
 //import type { NextApiRequest, NextApiResponse } from 'next'
 import { reciterSamlConfig }  from "../../../../config/saml"
-import { csrfToken } from "next-auth/core/lib/csrf";
+import { randomBytes } from "crypto";
 
 
 export default async function handler(req, res) {
@@ -11,19 +11,31 @@ export default async function handler(req, res) {
        /* const { data, headers } = await axios.get("/api/auth/csrf", {
             baseURL: "https://" + req.headers.host,
         });*/
-        const token = await csrfToken({ req });
+        const csrfToken = randomBytes(32).toString("hex");
         console.log('token********',csrfToken);
-        const res = await fetch(`${process.env.NEXTAUTH_URL}/api/auth/csrf`);
-        const { csrfToken1 } = await res.json();
+        const cookieValue = encodeURIComponent(csrfToken) + "|" + Date.now();
+        res.setHeader(
+            "Set-Cookie",
+            serialize("next-auth.csrf-token", cookieValue, {
+            httpOnly: true,
+            path: "/",
+            sameSite: "lax",
+            secure: process.env.NODE_ENV === "production",
+            maxAge: 60 * 60,
+            })
+        );
+
+     //   const res = await fetch(`${process.env.NEXTAUTH_URL}/api/auth/csrf`);
+     //   const { csrfToken1 } = await res.json();
 
        // console.log("data",data);
         //const { csrfToken } = data;
-        console.log('csrfToke********',csrfToken1);
+//      console.log('csrfToke********',csrfToken1);
         const encodedSAMLBody = encodeURIComponent(JSON.stringify(req.body));
         console.log('encodedSAMLBody',encodedSAMLBody);
         //res.setHeader("set-cookie", headers["set-cookie"] ?? "");
        // res.setHeader("Content-Type", "text/html");
-       res.setHeader("Set-Cookie", serialize("next-auth.csrf-token", cookieValue, {
+     /*  res.setHeader("Set-Cookie", serialize("next-auth.csrf-token", cookieValue, {
             httpOnly: true,
             path: "/",
             sameSite: "lax",
@@ -31,13 +43,13 @@ export default async function handler(req, res) {
             maxAge: 60 * 60
         }));
 
-        res.status(200).json({ csrfToken1 });
+        res.status(200).json({ csrfToken1 });*/
         
         return res.send(
             `<html>
           <body>
             <form action="/api/auth/callback/saml" method="POST">
-              <input type="hidden" name="csrfToken" value="${csrfToken1}"/>
+              <input type="hidden" name="csrfToken" value="${csrfToken}"/>
               <input type="hidden" name="samlBody" value="${encodedSAMLBody}"/>
             </form>
             <script>
