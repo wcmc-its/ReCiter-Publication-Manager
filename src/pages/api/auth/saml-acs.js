d7b3e5cf95edbf950dfe38b1b57f92b42b581b58
mrj4001 <mrj4001@med.cornell.edu>
2025-11-09 18:26:45 +0530

    made changes to use the SAML2-JS with NextJS 14 + NextAuth 4

--- src/pages/api/auth/saml-acs.js
+++ src/pages/api/auth/saml-acs.js
@@ -3,7 +3,7 @@
 import saml2 from "saml2-js";
 import { reciterSamlConfig }  from "../../../../config/saml"
 import { getServerSession } from "next-auth/next";
-import NextAuth from "./[...nextauth].jsx"; // import NextAuth instance
+//import {findOrcreateAdminUser,persistUserLogin} from "./[...nextauth].jsx"; // import NextAuth instance
 import { serialize } from "cookie";
 import axios from "axios";
 import { reciterConfig } from "../../../../config/local";
@@ -83,4 +83,134 @@ export default async function handler(req, res) {
     console.error("SAML ACS handler error:", err);
     return res.status(500).send("Internal Server Error");
   }
+}
+const findOrcreateAdminUser = async(cwid,samlEmail,samlFirstName,samlLastName) => {
+    const createdAdminUser = await findOrCreateAdminUsers(cwid,samlEmail,samlFirstName,samlLastName)
+    if(createdAdminUser)
+    {
+        await grantDefaultRolesToAdminUser(createdAdminUser);
+        await sleep(50);
+        let userRoles ='';
+         if(samlEmail)
+            userRoles = await findUserPermissions(samlEmail, "email")
+         else if(cwid)
+            userRoles = await findUserPermissions(cwid, "cwid")
+         createdAdminUser.userRoles = userRoles;
+          let databaseUser = {
+            "userID" : createdAdminUser.userID,
+            "personIdentifier": createdAdminUser.personIdentifier,
+            "nameFirst": createdAdminUser.firstName,
+            "nameMiddle": createdAdminUser.nameMiddle,
+            "nameLast":createdAdminUser.lastName,
+            "email" : createdAdminUser.samlEmail,
+            "status":createdAdminUser.status,
+            "createTimestamp":createdAdminUser.createTimestamp,
+            "modifyTimestamp":createdAdminUser.modifyTimestamp
+        }
+        createdAdminUser.databaseUser = databaseUser
+        createdAdminUser.personIdentifier 
+        if(createdAdminUser)
+            return createdAdminUser;
+    }
+    return createAdminUser;
+}
+const grantDefaultRolesToAdminUser = async(adminUser) => {
+    const adminSettings = await findOneAdminSettings('userRoles');
+    let assignRolesPayload =[];
+    if(adminSettings && adminSettings.viewAttributes && adminSettings.viewAttributes.length > 0)
+    {
+        let viewAttributes = JSON.parse(adminSettings.viewAttributes);
+        viewAttributes && viewAttributes.forEach(attr => {
+            attr.roles.map(role=>{
+                if(role.isChecked)
+                {
+                    let assignRolePayload = {
+                            'userID': (JSON.parse(JSON.stringify(adminUser))).userID,
+                            'roleID': role.roleId,
+                            'createTimestamp': new Date() 
+                            }
+                            //check for the role assigned to the user or not
+                            assignRolesPayload.push(assignRolePayload);
+                }
+            })
+        });
+
+    }
+    let personAPIResponse;
+    let existingAdminUserRoles =[];
+    let finalAssignRolesPayload =[];
+    if(adminUser && adminUser.personIdentifier)
+    {
+        personAPIResponse = await findOnePerson("personIdentifier",adminUser.personIdentifier);
+        existingAdminUserRoles = JSON.parse(await findUserPermissions(adminUser.personIdentifier, "cwid"))
+    } 
+    if(assignRolesPayload && assignRolesPayload.length >= 2)
+    {
+        //nothing continue
+    } 
+    //Check for Curator_All role in assignRolesPaylaod if it is there then continue otherwise, 
+    // check for an entry in person table with the personIdentifier, if exist then assign curator_self role 
+    else if(personAPIResponse && personAPIResponse.personIdentifier 
+        && ((assignRolesPayload && assignRolesPayload.length <=0) || (assignRolesPayload && assignRolesPayload.length >0 && !assignRolesPayload.some((role) => role.roleID == 2)))
+        && ((existingAdminUserRoles && existingAdminUserRoles.length <=0) || (existingAdminUserRoles && existingAdminUserRoles.length > 0 && (!existingAdminUserRoles.some((role) => role.roleLabel == allowedPermissions.Curator_All)
+                        && !existingAdminUserRoles.some((role) => role.roleLabel == allowedPermissions.Curator_Self )))))
+    {
+        let assignRolePayload = {
+            'userID': (JSON.parse(JSON.stringify(adminUser))).userID,
+            'roleID': 4,
+            'createTimestamp': new Date() 
+            }
+            //check for the role assigned to the user or not
+            assignRolesPayload.push(assignRolePayload);
+    }
+    //filtering the assignRolePayload with existingAdminUserRoles  if any 
+    if(existingAdminUserRoles && existingAdminUserRoles.length >0 && assignRolesPayload && assignRolesPayload.length >0)
+    {
+        finalAssignRolesPayload = assignRolesPayload.filter(value1 => !existingAdminUserRoles.some(value2 => value1.roleID === value2.roleID))
+    }
+    else if(assignRolesPayload && assignRolesPayload.length >0)
+    {
+        finalAssignRolesPayload = assignRolesPayload;
+    }
+
+    if(finalAssignRolesPayload && finalAssignRolesPayload.length > 0)
+    {
+        const userRole = await  findOrCreateAdminUserRole (finalAssignRolesPayload); 
+        return userRole;
+    }
+    //raise an error and display a message on the UI as "You have successfully authenticated, but you don't have any roles assigned. Please contact a system administrator".
+    else if(finalAssignRolesPayload && finalAssignRolesPayload.length <=0 && existingAdminUserRoles && existingAdminUserRoles.length <= 0)
+    {
+        return null;    
+    }
+    return existingAdminUserRoles;
+    
+}
+export const persistUserLogin =async (cwid)=>{
+    let payload = {
+        "cwid":  cwid,
+        "module":  "publication_manager"
+    }
+
+    let uri = `${reciterConfig.asms.userTrackingAPI}`
+    return fetch(uri, {
+            method: "POST",
+            headers: {
+                'Authorization': 'Bearer ' + reciterConfig.asms.userTrackingAPIAuthorization,
+            },
+            body: JSON.stringify(payload)
+        })
+            .then(async(res)=> {
+                console.log('ASMS User tracker end point api is Successfull: ', res)
+                if(res.status == 200) {
+                } 
+            })
+            .catch((error) => {
+                console.log('ASMS User tracker end point api is not reachable: ' + error)
+                return {
+                    statusCode: error.status,
+                    statusText: error
+                }
+            });
+            
 }
