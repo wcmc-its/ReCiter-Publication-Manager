d0e9e64c94254938dfb4856add6ea8320d72c129
veerraju <ved4006@med.cornell.edu>
2025-12-16 18:25:51 +0530

    url changes and saml-login.js

--- src/pages/api/auth/saml-login.js
+++ src/pages/api/auth/saml-login.js
@@ -1,26 +1,30 @@
-import { ServiceProvider } from 'saml2-js';
-import { reciterSamlConfig }  from "../../../../config/saml";
+import saml2 from "saml2-js"
+import axios from "axios"
+//import type { NextApiRequest, NextApiResponse } from 'next'
+import { reciterSamlConfig }  from "../../../../config/saml"
+
+
 
 export default async function handler(req, res) {
-  const sp = new saml2.ServiceProvider(reciterSamlConfig.saml_options);
-     const createLoginRequestUrl = (idp, options = {}) =>
-         new Promise((resolve, reject) => {
-             sp.create_login_request_url(idp, options, (error, loginUrl) => {
-                 if (error) {
-                     reject(error);
-                 }
-                 resolve(loginUrl);
-             });
-         });
- 
-     try {
-         const idp = new saml2.IdentityProvider(reciterSamlConfig.saml_idp_options);
-         const loginUrl = await createLoginRequestUrl(idp);
-         return res.redirect(loginUrl);
-     } catch (error) {
-         console.error(error);
-         res.status(500).send(error);
-     }
+ const sp = new saml2.ServiceProvider(reciterSamlConfig.samlOptions);
+    const createLoginRequestUrl = (idp, options = {}) =>
+        new Promise((resolve, reject) => {
+            sp.create_login_request_url(idp, options, (error, loginUrl) => {
+                if (error) {
+                    reject(error);
+                }
+                resolve(loginUrl);
+            });
+        });
+
+    try {
+        const idp = new saml2.IdentityProvider(reciterSamlConfig.idpOptions);
+        const loginUrl = await createLoginRequestUrl(idp);
+        return res.redirect(loginUrl);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error);
+    }
 
   
 }
