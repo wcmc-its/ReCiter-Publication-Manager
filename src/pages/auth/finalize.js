ff246db3fb20ab6e081db55590aca2eec639011f
mrj4001 <mrj4001@med.cornell.edu>
2026-01-07 13:45:55 +0530

    made changes for the SAML integration with NextAuth4 and NextJS14

--- src/pages/auth/finalize.js
+++ src/pages/auth/finalize.js
@@ -1,26 +1,27 @@
 import { useEffect } from 'react';
 import { signIn } from 'next-auth/react';
 
+export default function Finalize() {
+    useEffect(() => {
+    const performSignIn = async () => {
+        console.log("Attempting to trigger signIn...");
+        
+        // We set redirect: false to prevent the jump to the login page
+        // This allows us to see the error in the browser console
+        const result = await signIn('saml', { 
+        callbackUrl: '/search',
+        redirect: false 
+        });
 
-useEffect(() => {
-  const performSignIn = async () => {
-    console.log("Attempting to trigger signIn...");
-    
-    // We set redirect: false to prevent the jump to the login page
-    // This allows us to see the error in the browser console
-    const result = await signIn('saml', { 
-      callbackUrl: '/search',
-      redirect: false 
-    });
+        console.log("SignIn Result:", result);
 
-    console.log("SignIn Result:", result);
+        if (result?.error) {
+        console.error("Sign in failed with error:", result.error);
+        } else if (result?.ok) {
+        window.location.href = result.url;
+        }
+    };
 
-    if (result?.error) {
-      console.error("Sign in failed with error:", result.error);
-    } else if (result?.ok) {
-      window.location.href = result.url;
-    }
-  };
-
-  performSignIn();
-}, []);
+    performSignIn();
+    }, []);
+}
