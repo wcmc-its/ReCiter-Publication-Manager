95b14acb518a6122a04788f41d384ebf1213c83a
mrj4001 <mrj4001@med.cornell.edu>
2026-02-20 21:58:35 +0530

    Added AdminSettingsDataLoader component at app level to use from the state in all other components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -43,6 +43,10 @@ import Header from '../components/elements/Header/Header'
 import { Footer } from '../components/elements/Footer/Footer'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { useDispatch, useSelector } from 'react-redux';
+import { useSession } from 'next-auth/react';
+import { fetchAdminSettingsAction } from '../redux/actions/actions';
+import { useEffect } from 'react';
 
 
 // this should give a better typing
@@ -58,6 +62,21 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout
 }
 
+// This sub-component handles the logic so _app.tsx stays clean
+function AdminSettingsDataLoader() {
+    const dispatch = useDispatch();
+    const { data: session } = useSession();
+    const adminSettings = useSelector((state: any) => state.updatedAdminSettings || []);
+    useEffect(() => {
+        // If we have a session but NO settings in Redux, fetch them!
+        if (session && (!adminSettings || adminSettings.length === 0)) {
+            dispatch(fetchAdminSettingsAction());
+        }
+    }, [session, adminSettings, dispatch]);
+
+    return null; 
+}
+
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppPropsWithLayout) {
   const store = useStore(pageProps.initialReduxState)
   // Use the layout defined at the page level, if available
@@ -67,11 +86,14 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
   return (
     <SessionProvider session={session}>
         <ReduxProvider store={store}>
+        <AdminSettingsDataLoader/>  
         <Header/>
           {getLayout(<Component {...pageProps} />)}
           <Footer/>
         </ReduxProvider>
     </SessionProvider>
+
+    
   )
   
 }
