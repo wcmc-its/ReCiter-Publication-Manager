9fa3273eaf7788f7fff50849956b3d0d5b966e30
mrj4001 <mrj4001@med.cornell.edu>
2026-03-13 20:00:00 +0530

    reverted back saml2-js version changes as IDP is not working correctly

--- config/saml.ts
+++ config/saml.ts
@@ -6,11 +6,7 @@ export const reciterSamlConfig = {
       entity_id: process.env.ENTITY_ID, // Your SP entity ID
       certificate: fs.readFileSync(process.cwd() + "/config/certs/reciter-saml.crt").toString(),
       private_key: fs.readFileSync(process.cwd() + "/config/certs/reciter-saml.key").toString(),
-      sign_post_request: true,
-      allow_unencrypted_assertion: true,    // mimics old v3 behavior
-      force_authn: false,                   // mimics old v3 behavior
-      nameid_format: "urn:oasis:names:tc:SAML:2.0:nameid-format:transient",
-      skip_request_compression: true        // optional, matches v3 default
+      sign_get_request: true
     },
 
    idpOptions : {
