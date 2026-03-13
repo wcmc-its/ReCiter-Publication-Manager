4aa619e296459f09800dcbc037fa93edbe780805
mrj4001 <mrj4001@med.cornell.edu>
2026-03-13 18:14:52 +0530

    reverted back saml2-js version changes as IDP is not working correctly

--- src/pages/api/auth/saml-login.js
+++ src/pages/api/auth/saml-login.js
@@ -1,51 +1,16 @@
 import { ServiceProvider } from 'saml2-js';
 import { reciterSamlConfig }  from "../../../../config/saml";
-import zlib from "zlib";
 
 export default async function handler(req, res) {
   const sp = new ServiceProvider(reciterSamlConfig.samlOptions);
   const idp = reciterSamlConfig.idpOptions;
 
   sp.create_login_request_url(idp, {}, (err, loginUrl, requestId) => {
-
-  if (err) {
-    return res.status(500).send(err.message);
-  }
-  console.log("Full login URL:", loginUrl);
-
-  const samlRequest = new URL(loginUrl).searchParams.get("SAMLRequest");
-  console.log("Raw SAMLRequest (base64):", samlRequest);
-
- try {
-
-  const compressed = Buffer.from(samlRequest, "base64");
-
-  zlib.inflateRaw(compressed, (err, decoded) => {
-
-    if (err) {
-      console.error("Error inflating SAMLRequest:", err);
-    } else {
-      console.log("Decoded AuthnRequest XML:\n", decoded.toString());
-    }
-
+    if (err) return res.status(500).send(err.message);
+    const samlRequest = new URL(loginUrl).searchParams.get("SAMLRequest");
+    // Redirect user to IdP
+    res.redirect(loginUrl);
   });
 
-} catch (e) {
-  console.error("Failed decoding SAMLRequest", e);
-}
-  const form = `
-  <html>
-    <body onload="document.forms[0].submit()">
-      <form method="POST" action="${process.env.SSO_LOGIN_URL}">
-        <input type="hidden" name="SAMLRequest" value="${samlRequest}" />
-      </form>
-    </body>
-  </html>
-  `;
-
-  res.send(form);
-
-});
-  
   
 }
