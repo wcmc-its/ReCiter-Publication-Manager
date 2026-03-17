3da529e7ac3d9401f25a89ac68fadac36a6fa0d4
mrj4001 <mrj4001@med.cornell.edu>
2026-03-17 19:22:48 +0530

    Upgrading SAML2-js V3.0.0 to V4.0.4 for compliance

--- config/saml.ts
+++ config/saml.ts
@@ -6,7 +6,19 @@ export const reciterSamlConfig = {
       entity_id: process.env.ENTITY_ID, // Your SP entity ID
       certificate: fs.readFileSync(process.cwd() + "/config/certs/reciter-saml.crt").toString(),
       private_key: fs.readFileSync(process.cwd() + "/config/certs/reciter-saml.key").toString(),
-      sign_get_request: true
+      sign_get_request: true,
+      // 1. Set to true if your IdP doesn't encrypt the SAML response (common)
+  allow_unencrypted_assertion: true, 
+
+  // 2. Prevents "Assertion not yet valid" errors due to server clock drift (in seconds)
+  notbefore_skew: 2, 
+
+  // 3. Tells the IdP what format the username should be in
+  nameid_format: "urn:oasis:names:tc:SAML:2.0:nameid-format:transient",
+
+  // 4. Force SHA-256 for signing (modern standard)
+  signature_algorithm: "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256",
+  digest_algorithm: "http://www.w3.org/2001/04/xmlenc#sha256"
     },
 
    idpOptions : {
