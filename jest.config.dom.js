b90de71bbc439788b72bee5e1463450384c056db
paulalbert1 <3651640+paulalbert1@users.noreply.github.com>
2026-04-06 22:55:28 -0400

    chore(11-01): install @testing-library/dom, create jest.config.dom.js, update test script
    - Install @testing-library/dom@^10.4.1 peer dependency for @testing-library/react
    - Create jest.config.dom.js with jsdom environment for component tests
    - Narrow jest.config.js testMatch to exclude __tests__/components/
    - Update npm test script to run both configs sequentially
    - Fix plan's setupFilesAfterSetup to correct setupFilesAfterEnv key

--- jest.config.dom.js
+++ jest.config.dom.js
@@ -1,12 +1,13 @@
 module.exports = {
-  testEnvironment: 'node',
+  testEnvironment: 'jest-environment-jsdom',
   transform: {
     '^.+\\.[jt]sx?$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
-  testMatch: ['<rootDir>/__tests__/*.test.(ts|tsx|js)'],
+  testMatch: ['<rootDir>/__tests__/components/**/*.test.(ts|tsx|js)'],
   moduleNameMapper: {
     '\\.module\\.css$': '<rootDir>/__tests__/__mocks__/styleMock.js',
     '\\.css$': '<rootDir>/__tests__/__mocks__/styleMock.js',
   },
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
 }
